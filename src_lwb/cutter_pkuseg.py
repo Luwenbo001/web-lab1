57a9f173026ca4276c37fb2740a10378be225837
Luwenbo <2154674187@qq.com>
2024-11-18 22:55:28 +0800

    lwb_v0.11

--- src_lwb/cutter_pkuseg.py
+++ src_lwb/cutter_pkuseg.py
@@ -16,36 +16,36 @@ movie_output_path = "../data/selected_movie_top_1200_data_tag_tokenized_pkuseg.c
 
 seg = pkuseg.pkuseg()
 
-for i,member in enumerate(book_reader):
-    if(i==0): continue
-    is_pair = 0
-    str = ""
+# for i,member in enumerate(book_reader):
+#     if(i==0): continue
+#     is_pair = 0
+#     str = ""
 
-    book_output_row = ""
-    book_output_row += member[0] + "," + '"' + "{"
+#     book_output_row = ""
+#     book_output_row += member[0] + "," + '"' + "{"
 
-    words = []
-    # print(member[1])
-    for ch in member[1]:
-        if((ch == "," or ch == '}')and is_pair == 1): 
-            is_pair = 0
-            words.append(str[:-1])
-            str = ""
-            continue
-        if(is_pair == 1):
-            str += ch
-        if(ch == "'" and is_pair == 0): is_pair = 1
-    token = []
-    for word in words:
-        tokens = seg.cut(word)
-        for token in tokens:
-            book_output_row += "'" + token + "'" + ","
-    book_output_row += "}" + '"' + "\n"
-    book_content.append(book_output_row)
+#     words = []
+#     # print(member[1])
+#     for ch in member[1]:
+#         if((ch == "," or ch == '}')and is_pair == 1): 
+#             is_pair = 0
+#             words.append(str[:-1])
+#             str = ""
+#             continue
+#         if(is_pair == 1):
+#             str += ch
+#         if(ch == "'" and is_pair == 0): is_pair = 1
+#     token = []
+#     for word in words:
+#         tokens = seg.cut(word)
+#         for token in tokens:
+#             book_output_row += "'" + token + "'" + ","
+#     book_output_row += "}" + '"' + "\n"
+#     book_content.append(book_output_row)
 
-with open(book_output_path, 'w') as f:
-    f.write(book_column + "\n")
-    f.writelines(book_content)
+# with open(book_output_path, 'w') as f:
+#     f.write(book_column + "\n")
+#     f.writelines(book_content)
 
 
 
@@ -72,7 +72,7 @@ for i,member in enumerate(movie_reader):
     for word in words:
         tokens = seg.cut(word)
         for token in tokens:
-            book_output_row += "'" + token + "'" + ","
+            movie_output_row += "'" + token + "'" + ","
     movie_output_row += "}" + '"' + "\n"
     movie_content.append(movie_output_row)
 
