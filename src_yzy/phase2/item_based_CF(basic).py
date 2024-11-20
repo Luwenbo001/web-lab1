95893eb467872cba65a8b4d32f07418925a31e79
yzyatghb <3068056506@qq.com>
2024-11-20 18:35:35 +0800

    basic_item_based_prediction

--- src_yzy/phase2/item_based_CF(basic).py
+++ src_yzy/phase2/item_based_CF(basic).py
@@ -1,21 +1,28 @@
 import math
+import csv
 
 # for data format:
 #       user1   user2   user3
 # item1 
 # item2
 # item3                         for example
-# {{item1, {user1 : comment11, user2 : comment12...}},
-#  {item2, {user1 : comment21, user2 : comment22...}},
-#  {item3, {user1 : comment31, user2 : comment32...}},
+# {{item1: {user1 : comment11, user2 : comment12...}},
+#  {item2: {user1 : comment21, user2 : comment22...}},
+#  {item3: {user1 : comment31, user2 : comment32...}},
 #  ...}
+
+input_train = "../../data/train_book_score.csv"
+input_test = "../../data/test_book_score.csv"
+output_path = "../../data/predicted_rank.txt"
 
 class item_comments():
     
     def __init__(self, data):
         self.data = data
         self.aver = {}
-        for item, comment in self.data.items():
+        for i, (item, comment) in enumerate(self.data.items()):
+            if i == 0:
+                continue
             item_sum = 0.0  #评分总和
             com_sum = 0     #评分总数
             for user, value in comment.items():
@@ -59,9 +66,9 @@ class item_comments():
         for item_, comment in self.data.items():
             if (item == item_):
                 continue
-            if (comment[user] != -1):
+            if (user in comment):
                 p_s = self.pearson_sim(item, item_)
-                numerator += p_s * comment[user]
+                numerator += p_s * float(comment[user])
                 denomintor += p_s
         
         if denomintor == 0:
@@ -78,12 +85,49 @@ class item_comments():
         return ret_data
 
 def import_data():
-    return
+    data = {}
+    with open(input_train, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for comment in reader:
+            user = comment[0]
+            item = comment[1]
+            value = comment[2]
+            item_set = data.setdefault(item, {})
+            item_set[user] = value
+    # print(data)
+    return data
     
+def test_predict(comment_set : item_comments):
+    user_comments = {}
+    for item, comments in comment_set.data.items():
+        for user, value in comments.items():
+            user_comment = user_comments.setdefault(user, {})
+            user_comment[item] = value
+            
+    with open(input_test, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for i, comment in enumerate(reader):
+            if (i > 2000):
+                break
+            if i == 0:
+                continue
+            user_ = comment[0]
+            item_ = comment[1]
+            predict_value = comment_set.predict_rank(item_, user_)
+            user_comments[user][item_] = predict_value
+            print(user_, item_, predict_value)
+    
+    for user, comments in user_comments.items():
+        comments = sorted(comments.items(), key=lambda d: d[1], reverse=True)
+    
+    with open(output_path, 'r', encoding='utf-8') as output_file:
+        for user, comments in user_comments.items():
+            print(user, comments, file=output_file)
+            
 def main():
     data = import_data()
     comment_set = item_comments(data=data)
-    predict_set = comment_set.solution()
+    test_predict(comment_set)
     
 if __name__ == '__main__':
     main()
